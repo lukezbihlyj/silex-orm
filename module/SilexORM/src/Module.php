4288e0fdeb87ff3c88fd8c454168b03eb5a85661
Luke Zbihlyj <me@lukezbihlyj.co.uk>
2016-04-09 16:08:41 +0100

    Remove unnecessary dependency and give control over Spot2 package.

--- module/SilexORM/src/Module.php
+++ module/SilexORM/src/Module.php
@@ -4,7 +4,9 @@ namespace LukeZbihlyj\SilexORM;
 
 use LukeZbihlyj\SilexPlus\Application;
 use LukeZbihlyj\SilexPlus\ModuleInterface;
-use Dijky\Silex\Provider\SpotServiceProvider;
+use Spot\Locator;
+use Spot\Config;
+use Pimple;
 
 /**
  * @package LukeZbihlyj\SilexORM\Module
@@ -24,10 +26,37 @@ class Module implements ModuleInterface
      */
     public function init(Application $app)
     {
-        $app->register(new SpotServiceProvider(), [
-            'spot.connections' => [
-                'main' => $app['database.dsn']
-            ]
-        ]);
+        $app['spot.connections.default'] = null;
+
+        $app['spot.connections'] = [
+            'main' => $app['database.dsn']
+        ];
+
+        $app['spot'] = $app->share(function() use ($app) {
+            return new Locator($app['spot.config']);
+        });
+
+        $app['spot.config'] = $app->share(function() use ($app) {
+            $config = new Config();
+            $connections = $app['spot.connections'];
+
+            if ($connections instanceof Pimple) {
+                $keys = $connections->keys();
+            } else {
+                $keys = array_keys($connections);
+            }
+
+            foreach ($keys as $key) {
+                if (isset($app['spot.connections.default']) && $key === $app['spot.connections.default']) {
+                    $default = true;
+                } else {
+                    $default = false;
+                }
+
+                $config->addConnection($key, $connections[$key], $default);
+            }
+
+            return $config;
+        });
     }
 }
